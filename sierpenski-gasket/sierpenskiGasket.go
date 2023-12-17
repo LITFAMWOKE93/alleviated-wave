a8a19eb095acc7382f880ff26515d516b5e024c6
Sam Trafton <samatrafton@gmail.com>
2023-12-17 11:39:37 +0200

    can init windows for diff projects

--- sierpenski-gasket/sierpenskiGasket.go
+++ sierpenski-gasket/sierpenskiGasket.go
@@ -1,1 +1,40 @@
-package sierpenskigasket
+package main
+
+import (
+	"LITFAMWOKE93/alleviated-wave/common"
+	"runtime"
+	"time"
+
+	"github.com/go-gl/glfw/v3.3/glfw"
+)
+
+var (
+// a slice of float32, the datatype that is always fed to openGL
+// 0, 0, 0 is the center axis of the view
+
+)
+
+const (
+	FPS = 60
+)
+
+func main() {
+	runtime.LockOSThread()
+
+	window := common.InitGlfw("Sierpenski Gasket")
+
+	defer glfw.Terminate()
+	program := common.InitOpenGL()
+
+	// Make the object
+	t := time.Now()
+	for !window.ShouldClose() {
+
+		if err := common.Draw(program, window); err != nil {
+			panic(err)
+		}
+		// create an FPS
+		time.Sleep(time.Second/time.Duration(FPS) - time.Since(t))
+	}
+
+}
